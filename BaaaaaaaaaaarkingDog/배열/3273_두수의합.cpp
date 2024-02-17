a4536f9f3e3c4fdef4764400aa05b0db81ffce39
GCgang <powm97@gmail.com>
2024-02-18 03:42:08 +0900

    [cpp] solve baekjoon 3273

---
+++ BaaaaaaaaaaarkingDog/배열/3273_두수의합.cpp
@@ -0,0 +1,55 @@
+// https://www.acmicpc.net/problem/3273
+/*
+문제:
+n개의 서로 다른 양의 정수 a1, a2, ... an으로 이루어진 수열이 있다.
+ai의 값은 1보다 크거나 같고,
+1000000보다 작거나 같은 자연수이다.
+자연수 x가 주어졌을 때, ai + aj = x(1 <= i < j <= n)을 만족하는 (ai, aj)쌍의 수를 구하는 프로그램을 작성하시오
+
+입력:
+첫째 줄에 수열의 크기 n이 주어진다.
+다음 줄에는 수열에 포함되는 수가 주어진다.
+셋째 줄에는 x가 주어진다 (1 <= n <= 100000, 1<= x <= 2000000)
+
+출력:
+문제의 조건을 만족하는 쌍의 개수를 출력한다
+
+ex:
+입력
+9
+5 12 7 10 9 1 2 3 11
+13
+
+출력
+3
+*/
+
+#include <iostream>
+using namespace std;
+
+int n, x, ret;
+int nums[1000001];
+bool check[2000001];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+    cin >> x;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if ((x - nums[i]) > 0 && check[x - nums[i]]) // x에서 nums에 있는 수를 뺀 값이 체크 배열에 있다면 짝이 맞다는 거임
+            ret++;
+        check[nums[i]] = true;
+    }
+
+    cout << ret << "\n";
+    return 0;
+}
