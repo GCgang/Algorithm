632f515695a66c1e88af965d0de9e35b362b854d
GCgang <powm97@gmail.com>
2024-10-06 16:48:22 +0900

    [js] solve programmers lv01 숫자 문자열과 영단어

---
+++ javascript/programmers/lv01/숫자 문자열과 영단어.js
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+
+function solution(s) {
+  const numberMap = {
+    zero: '0',
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  };
+
+  for (const [word, digit] of Object.entries(numberMap)) {
+    s = s.split(word).join(digit);
+  }
+  return parseInt(s);
+}
