5898141f7040dd646d8d1b6762f1e5078426a743
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-18 04:41:48 +0900

    [js] insertion sort

---
+++ javascript/쉽게설명한 자바스크립트 알고리즘/정렬 알고리즘/InsertionSort/InsertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = key;
+  }
+  return array;
+}
+
+console.log(insertionSort([5, 3, 8, 4, 2]));
