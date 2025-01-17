59f41a3b260a3532fd550825e0425ef17f5e4f44
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-18 04:41:38 +0900

    [js] selection sort

---
+++ javascript/쉽게설명한 자바스크립트 알고리즘/정렬 알고리즘/SelectionSort/SelectionSort.js
@@ -0,0 +1,14 @@
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [array[i], array[minIndex]] = [array[minIndex], array[i]];
+  }
+  return array;
+}
+
+console.log(selectionSort([2, 1, 4, 3, 6]));
