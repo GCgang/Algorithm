491a72a7d8ba26f4277dedd5eb7aa7cdac2290a6
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-22 20:22:06 +0900

    [js] solve 문자열 내림차순으로 배치하기

---
+++ javascript/programmers/lv01/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12917
+function solution(s) {
+  return s
+    .split('')
+    .sort((a, b) => {
+      if (a < b) return 1;
+      if (a > b) return -1;
+      return 0;
+    })
+    .join('');
+}
+
+// function solution(s) {
+//   return s
+//     .split('')
+//     .sort()
+//     .reverse()
+//     .join('');
+// }
