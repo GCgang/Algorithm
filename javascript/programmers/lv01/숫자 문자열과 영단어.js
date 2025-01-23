e951b0859d16bb53f264e539bc94c3230e91941d
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-23 18:33:54 +0900

    [js] solve 숫자 문자열과 영단어

--- javascript/programmers/lv01/숫자 문자열과 영단어.js
+++ javascript/programmers/lv01/숫자 문자열과 영단어.js
@@ -19,3 +19,24 @@ function solution(s) {
   }
   return parseInt(s);
 }
+
+function solution2(s) {
+  const numberMap = {
+    zero: '0',
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  };
+
+  return Number(
+    s.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (match) => {
+      return numberMap[match];
+    })
+  );
+}
