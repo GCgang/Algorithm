cb3e3950d1f92c74d0ac68035fa34695e1524616
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-02-23 12:29:23 +0900

    [js] solve 점프와 순간 이동

---
+++ javascript/programmers/lv02/점프와 순간 이동.js
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12980
+function solution(n) {
+  let count = 0;
+
+  while (n > 0) {
+    if (n % 2 === 1) {
+      count++;
+    }
+    n = Math.floor(n / 2);
+  }
+
+  return count;
+}
